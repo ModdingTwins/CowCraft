f9257305f6026b0ed0234377620fcc453758c8c2
MTwins <42128152+ModdingTwins@users.noreply.github.com>
2018-10-30 21:43:29 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,33 @@ const Discord = require("discord.js")
 const bot = new Discord.Client()
 bot.on("ready", () => {
 console.log(`${bot.user.username}` + "is online")
+
+bot.user.setActivity("Discord Community", {type: "STREAMING"})
+});
+bot.login(process.env.TOKEN)
+
+bot.on("message", (message) => {
+if(message.content === "~ip") {
+message.channel.send("twinfactions.tk : 19132")
+}
+});
+
+bot.on("message", (message) => {
+if(message.content === "~youtube") {
+message.channel.send("YouTube.com/c/ModdingTwinz")
+ }
+ });
  
 bot.on("message", (message) => {
-if(message.content === "¿help") {
+if(message.content === "~discord") {
+message.channel.send("https://discord.gg/u3vqvst")
+}
+});
+ 
+bot.on("message", (message) => {
+if(message.content === "~help") {
 console.log("Log")
-message.channel.send(`${message.author.username}` + ", I Have Sent You The Commands 📪")
-message.author.send("Please specify a module that you would like to look up:\n\n\n Powered By ``ModdingTwinz#4560``")
+message.channel.send(`${message.author.username}` + ", I Have Sent You The Commands ðŸ“ª")
+message.author.send("Please specify a module that you would like to look up:\n 1. ~ip ``TwinFactions Server Ip & Port``\n 2. ~youtube ``Subscribe To ModdingTwinz Channel``\n 3. ~discord ``Join us on TwinFactions & Have Fun!``\n\n Powered By ``ModdingTwinz#4560``")
 }
 });
