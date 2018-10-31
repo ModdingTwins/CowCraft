2bb14e9bd5cae2f5daa2795651c2de8ed10b5618
MTwins <42128152+ModdingTwins@users.noreply.github.com>
2018-10-30 20:57:00 -0400

    Update index.js

--- index.js
+++ index.js
@@ -8,27 +8,27 @@ bot.user.setActivity("Discord Community", {type: "STREAMING"})
 bot.login(process.env.TOKEN)
 
 bot.on("message", (message) => {
-if(message.content === "~ip") {
+if(message.content === "-ip") {
 message.channel.send("twinfactions.tk : 19132")
 }
 });
 
 bot.on("message", (message) => {
-if(message.content === "~youtube") {
+if(message.content === "-youtube") {
 message.channel.send("YouTube.com/c/ModdingTwinz")
  }
  });
  
 bot.on("message", (message) => {
-if(message.content === "~discord") {
+if(message.content === "-discord") {
 message.channel.send("https://discord.gg/u3vqvst")
 }
 });
  
 bot.on("message", (message) => {
-if(message.content === "") {
+if(message.content === "-help") {
 console.log("Log")
 message.channel.send(`${message.author.username}` + ", I Have Sent You The Commands 📪")
-message.author.send("Please specify a module that you would like to look up:\n 1. ~ip ``TwinFactions Server Ip & Port``\n 2. ~youtube ``Subscribe To ModdingTwinz Channel``\n 3. ~discord ``Join us on TwinFactions & Have Fun!``\n\n Powered By ``Discord``")
+message.author.send("Please specify a module that you would like to look up:\n 1. -ip ``TwinFactions Server Ip & Port``\n 2. -youtube ``Subscribe To ModdingTwinz Channel``\n 3. -discord ``Join us on TwinFactions & Have Fun!``\n\n Powered By ``Discord``")
 }
 });
