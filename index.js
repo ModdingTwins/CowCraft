eeef9ef82c92f736e1896c28eb981ab11aa030f9
MTwins <42128152+ModdingTwins@users.noreply.github.com>
2018-10-31 21:56:18 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-client.on('ready', () => {
-  console.log('I am ready!');
-});
-client.on('guildMemberAdd', member => {
-  const channel = member.guild.channels.find(ch => ch.name === 'welcome');
- if (!channel) return;
-  channel.send(`Welcome to the server, ${member}`);
-});
+const Discord = require("discord.js")
+const bot = new Discord.Client()
+bot.on("ready", () => {
+console.log(`${bot.user.username}` + "is online")
 
-client.login('NTA2ODQ4NDc1NTM1NzY5NjAy.DrvtXg.WFGbPSYwBlx4C7Ztlmi2hO4hfXw');
+bot.user.setActivity("Discord Community", {type: "STREAMING"})
+});
+bot.login(process.env.TOKEN)
+
+bot.on("message", (message) => {
+if(message.content === "-help") {
+console.log("Log")
+message.channel.send(`${message.author.username}` + ", I Have Sent You The Commands 📭")
+message.author.send("Please specify a module that you would like to look up:\n\n Powered By ``ModdingTwinz#4560``")
+}
+});
